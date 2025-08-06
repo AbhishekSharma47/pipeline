133bc7bb272f95fcb133e3e9be4b84285d572907
AbhishekSharma47 <abhishek.sharma199147@gmail.com>
2025-08-06 23:22:38 +0530

    Update Backend.tf

--- Backend.tf
+++ Backend.tf
@@ -5,13 +5,6 @@ terraform {
       version = "4.38.1"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "backend"
-    storage_account_name = "abackend"
-    container_name       = "backend"
-    key                  = "bknd.tfstate"
-    subscription_id      = "661ea49e-05d4-429e-8204-de0d46c539b5"
-  }
 }
 
 provider "azurerm" {
